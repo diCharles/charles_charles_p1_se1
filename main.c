cbc33b82cf1bc1fbaf455f5279d85e692f399bbc
diCharles <43318661+diCharles@users.noreply.github.com>
2019-03-21 15:37:45 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -20,6 +20,18 @@ typedef struct{
 	uint8_t enable_generator:1;
 }flags_t;
 
+void system_control();
+/*FLAGS FOR RUN PROCESS*/
+	uint8_t g_run_motor=FALSE;
+	uint8_t g_run_generator=FALSE;
+	/*flag for stop running the process motor and generator after press A or B and insert corresponding password*/
+	uint8_t g_stop_motor=FALSE;
+	uint8_t g_stop_generator =FALSE;
+	/*this flag will be set everytime a password correct is found*/
+	uint8_t g_a_correct_password = FALSE;
+	/* the passwords are checked on the pit3 flag each 200ms aproximately*/
+	static uint8_t g_check_keyboard = FALSE;
+
 flags_t g_activation_by_A_or_B_keys;
 int main(void) {
 
@@ -30,165 +42,162 @@ int main(void) {
 	init_rgb();
 
 	init_password();/* init  interrupt pind0 conected to and gate, pit 3 for debounce and keyboard*/
-	/* the passwords are checked on the pit3 flag each 200ms aproximately*/
-	uint8_t a_correct_password = FALSE;
-	static uint8_t check_keyboard = FALSE;
+
 	/* initialization of signal generator*/
 	init_generador_seniales();
 	/*initialization of motor*/
 	motor_init();
 	/*set all cols in zero enables for a interrupt by any keyboard key*/
 	set_columns_value(0);
-	/*FLAGS FOR RUN PROCESS*/
-	uint8_t run_motor=FALSE;
-	uint8_t run_generator=FALSE;
-	/*flag for stop running the process motor and generator after press A or B and insert corresponding password*/
-	uint8_t stop_motor=FALSE;
-	uint8_t stop_generator =FALSE;
+
 
 	while(1) {
 
-		/*if any key is pressed he and gate will be zero, the and interrupt on pin PD0 is launched*/
-		if(TRUE == GPIO_get_irq_status(GPIO_D))
-		{
-			/* read the keyboard whenever its possible*/
-			check_keyboard=TRUE;
-			/*clear interrupt flag*/
+
+		 system_control();
 
 
-		}
-		/*the pit is used for debounce of keyboard*/
-		if(PIT_get_interrupt_flag_status(PIT_3))
-		{
-			/*if check_keyboard was set by interrupt */
-			if(TRUE == check_keyboard)
-			{   /*the pit has a periodic interrupt each 100ms*/
-				/*the keyboard is read at least in 100 ms or in 200ms at max*/
-				static uint8_t at_least_2_delays=0;
-				at_least_2_delays++;
-				if (2 == at_least_2_delays)
-				{
-					/*two delays for the pit already pass,lets read the keyboard*/
-					uint8_t read =read_keyboard();
-					/*set all cols in zero enables for a interrupt by any keyboard key*/
-					set_columns_value(0);
-					/*here the 3 passwords are checked reading the last key pressed by user */
-					a_correct_password =check3Passwords(get_lastKey() );/*if correct password appears his signal will be stort*/
-					/*the key has been processed, its time to clean it*/
-					clear_lastKey();
-					/*enable for a new keyboard read with debounce with pit*/
-					at_least_2_delays=0;
-					/* generator process is or disabled whenever  key B is pressed*/
-					if(keyB ==read      && (TRUE == get_sgnal_generator_status()) && (TRUE == get_system_status()) )
-					{
-						/*every time key B is pressed generator activation will be  toggled*/
-						if(FALSE == run_generator)
-						{	/*if the  generator was not running*/
-							g_activation_by_A_or_B_keys.enable_generator=TRUE;
-						}
-						else
-						{//when the motor generator was running
-							g_activation_by_A_or_B_keys.enable_generator=FALSE;/* flag to enable it is cleared*/
-
-						}
-					}
-					else if((keyA ==read) && (TRUE == get_motor_status()) && (TRUE == get_system_status()))
-					{
-						if(FALSE == run_motor)
-						{
-							g_activation_by_A_or_B_keys.enable_motor=TRUE;
-
-						}
-						else
-						{
-							g_activation_by_A_or_B_keys.enable_motor=FALSE;
-						}
-					}
-					/*clearing flag for read the keyboard, it must be set again by interrupt on port D*/
-					check_keyboard=FALSE;
-					GPIO_clear_irq_status(GPIO_D);
-					/*the system will be enabled the the password 1234 was ingresed*/
-					if(TRUE == get_system_status())
-					{
-						/* here the system is activated*/
-						rgb_color(RED,ON);/*led indicating activation of system*/
-
-						if(TRUE == get_motor_status())
-						{
-
-							/*here motor is activated but not enabled*/
-							if(TRUE == g_activation_by_A_or_B_keys.enable_motor)
-							{
-								/* motor proccess activated*/
-								rgb_color(GREEN,ON);/*led indicating activation of system*/
-								run_motor=TRUE;
-								//FSM_motor();
-							}
-
-
-						}
-						else
-						{
-							if(FALSE == g_activation_by_A_or_B_keys.enable_motor)
-							{
-								/* stoping the generator*/
-								rgb_color(GREEN,OFF);
-								run_motor=FALSE;
-
-							}
-						}
-
-
-						if(TRUE == get_sgnal_generator_status() )
-						{
-							/* here signal generator is activated but no enabled*/
-							if(TRUE == g_activation_by_A_or_B_keys.enable_generator)
-							{
-								rgb_color(BLUE,ON);/*led indicating activation of system*/
-								run_generator=TRUE;
-								stop_generator=FALSE;
-							}
-
-
-						}
-						else
-						{
-							/* here signal generator is activated but no enabled*/
-							if(FALSE == g_activation_by_A_or_B_keys.enable_generator)
-							{
-								/* stoping the generator*/
-								rgb_color(BLUE,OFF);
-								run_generator=FALSE;
-
-							}
-						}
-
-
-					}
-					else
-					{
-						/*the system is disaled here*/
-						rgb_color(RED,OFF);
-					}
-
-				}
-			}
-
-			PIT_clear_interrupt_flag(PIT_3);
-		}
-
-
-
-
-		if( TRUE ==run_generator)
+		if( TRUE ==g_run_generator)
 		{
 			generador_seniales();
 		}
-		if(TRUE == run_motor)
+		if(TRUE == g_run_motor)
 		{
 			FSM_motor();
 		}
 
 	}
 	return 0 ;
+}
+void system_control()
+{
+
+	/*if any key is pressed he and gate will be zero, the and interrupt on pin PD0 is launched*/
+	if(TRUE == GPIO_get_irq_status(GPIO_D))
+	{
+		/* read the keyboard whenever its possible*/
+		g_check_keyboard=TRUE;
+		/*clear interrupt flag*/
+
+
+	}
+	/*the pit is used for debounce of keyboard*/
+	if(PIT_get_interrupt_flag_status(PIT_3))
+	{
+		/*if g_check_keyboard was set by interrupt */
+		if(TRUE == g_check_keyboard)
+		{   /*the pit has a periodic interrupt each 100ms*/
+			/*the keyboard is read at least in 100 ms or in 200ms at max*/
+			static uint8_t at_least_2_delays=0;
+			at_least_2_delays++;
+			if (2 == at_least_2_delays)
+			{
+				/*two delays for the pit already pass,lets read the keyboard*/
+				uint8_t read =read_keyboard();
+				/*set all cols in zero enables for a interrupt by any keyboard key*/
+				set_columns_value(0);
+				/*here the 3 passwords are checked reading the last key pressed by user */
+				g_a_correct_password =check3Passwords(get_lastKey() );/*if correct password appears his signal will be stort*/
+				/*the key has been processed, its time to clean it*/
+				clear_lastKey();
+				/*enable for a new keyboard read with debounce with pit*/
+				at_least_2_delays=0;
+				/* generator process is or disabled whenever  key B is pressed*/
+				if(keyB ==read      && (TRUE == get_sgnal_generator_status()) && (TRUE == get_system_status()) )
+				{
+					/*every time key B is pressed generator activation will be  toggled*/
+					if(FALSE == g_run_generator)
+					{	/*if the  generator was not running*/
+						g_activation_by_A_or_B_keys.enable_generator=TRUE;
+					}
+					else
+					{//when the motor generator was running
+						g_activation_by_A_or_B_keys.enable_generator=FALSE;/* flag to enable it is cleared*/
+
+					}
+				}
+				else if((keyA ==read) && (TRUE == get_motor_status()) && (TRUE == get_system_status()))
+				{
+					if(FALSE == g_run_motor)
+					{
+						g_activation_by_A_or_B_keys.enable_motor=TRUE;
+
+					}
+					else
+					{
+						g_activation_by_A_or_B_keys.enable_motor=FALSE;
+					}
+				}
+				/*clearing flag for read the keyboard, it must be set again by interrupt on port D*/
+				g_check_keyboard=FALSE;
+				GPIO_clear_irq_status(GPIO_D);
+				/*the system will be enabled the the password 1234 was ingresed*/
+				if(TRUE == get_system_status())
+				{
+					/* here the system is activated*/
+					rgb_color(RED,ON);/*led indicating activation of system*/
+
+					if(TRUE == get_motor_status())
+					{
+
+						/*here motor is activated but not enabled*/
+						if(TRUE == g_activation_by_A_or_B_keys.enable_motor)
+						{
+							/* motor proccess activated*/
+							rgb_color(GREEN,ON);/*led indicating activation of system*/
+							g_run_motor=TRUE;
+							//FSM_motor();
+						}
+
+
+					}
+					else
+					{
+						if(FALSE == g_activation_by_A_or_B_keys.enable_motor)
+						{
+							/* stoping the generator*/
+							rgb_color(GREEN,OFF);
+							g_run_motor=FALSE;
+
+						}
+					}
+
+
+					if(TRUE == get_sgnal_generator_status() )
+					{
+						/* here signal generator is activated but no enabled*/
+						if(TRUE == g_activation_by_A_or_B_keys.enable_generator)
+						{
+							rgb_color(BLUE,ON);/*led indicating activation of system*/
+							g_run_generator=TRUE;
+							g_stop_generator=FALSE;
+						}
+
+
+					}
+					else
+					{
+						/* here signal generator is activated but no enabled*/
+						if(FALSE == g_activation_by_A_or_B_keys.enable_generator)
+						{
+							/* stoping the generator*/
+							rgb_color(BLUE,OFF);
+							g_run_generator=FALSE;
+
+						}
+					}
+
+
+				}
+				else
+				{
+					/*the system is disaled here*/
+					rgb_color(RED,OFF);
+				}
+
+			}
+		}
+
+		PIT_clear_interrupt_flag(PIT_3);
+	}
 }
